c3ff3442d2d05f4c8f67000a4c9a71b8e5afac61
GastonF5 <fr.gaston5@gmail.com>
2024-06-05 12:48:22 +0200

    Décoration EntityBars

--- scripts/EntityBars.gd
+++ scripts/EntityBars.gd
@@ -5,3 +5,7 @@ extends Control
 @export var hp_bar: TextureProgressBar
 @export var pa_bar: TextureProgressBar
 @export var pm_bar: TextureProgressBar
+
+
+func _ready():
+	$Decoration.position.y += 10
