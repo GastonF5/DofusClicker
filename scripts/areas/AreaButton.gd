2d7538094bf7b1f983ca7fb67bed277825191fa3
GastonF5 <fr.gaston5@gmail.com>
2024-07-09 20:07:28 +0200

    Improve AreaButton

--- scripts/areas/AreaButton.gd
+++ scripts/areas/AreaButton.gd
@@ -5,7 +5,8 @@ extends PanelContainer
 const area_btn_scene = preload("res://scenes/areas/area_button.tscn")
 
 @export var _icon_txt: TextureRect
-@export var _label: Label
+@export var _area_name_lbl: Label
+@export var _level_lbl: Label
 @export var _btn: Button
 
 var new := true
@@ -20,7 +21,11 @@ signal button_up
 func init(area_res: AreaResource, icon_txt: Texture2D):
 	area_id = area_res._id
 	name = str(area_id)
-	_label.text = area_res._name
+	_area_name_lbl.text = area_res._name
+	if area_res._level > 0:
+		_level_lbl.text = "Niveau %d" % area_res._level
+	else:
+		_level_lbl.visible = false
 	_icon_txt.texture = icon_txt
 
 
@@ -35,5 +40,5 @@ static func create(area_res: AreaResource, callable: Callable, icon_txt: Texture
 func _on_button_button_up():
 	clicked_callable.call(area_id)
 	if !is_dungeon and new:
-		_icon_txt.texture = null
+		_icon_txt.visible = false
 		new = false
