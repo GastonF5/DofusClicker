bf0ac6ed3340131284bf25835afc65a37715ce1b
GastonF5 <fr.gaston5@gmail.com>
2024-09-08 16:06:45 +0200

    #65 Forcer la vue "Infos" en combat

--- scripts/CustomTabContainer.gd
+++ scripts/CustomTabContainer.gd
@@ -4,10 +4,14 @@ extends TabContainer
 
 @export var tab_bar: TabBar
 
+var _tab_before_fight := -1
 
 func _ready():
 	tab_bar.tab_changed.connect(_on_tab_bar_tab_changed)
 	current_tab = tab_bar.current_tab
+	if tab_bar.name == "LeftTabBar":
+		GameManager.start_fight.connect(_on_fight_started)
+		GameManager.end_fight.connect(_on_fight_ended)
 
 
 func _on_tab_bar_tab_changed(tab):
@@ -16,3 +20,14 @@ func _on_tab_bar_tab_changed(tab):
 
 func _on_tab_bar_active_tab_rearranged(idx_to):
 	move_child(get_child(current_tab), idx_to)
+
+
+func _on_fight_started():
+	tab_bar.visible = false
+	_tab_before_fight = tab_bar.current_tab
+	tab_bar.current_tab = 2
+
+
+func _on_fight_ended():
+	tab_bar.visible = true
+	tab_bar.current_tab = _tab_before_fight
