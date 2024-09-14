261493c6c8483f2dc517ff7deffe600630fb96fa
GastonF5 <fr.gaston5@gmail.com>
2024-09-14 17:40:28 +0200

    #65 Rester sur la vue infos en fin de combat

--- scripts/CustomTabContainer.gd
+++ scripts/CustomTabContainer.gd
@@ -4,8 +4,6 @@ extends TabContainer
 
 @export var tab_bar: TabBar
 
-var _tab_before_fight := -1
-
 func _ready():
 	tab_bar.tab_changed.connect(_on_tab_bar_tab_changed)
 	current_tab = tab_bar.current_tab
@@ -24,10 +22,8 @@ func _on_tab_bar_active_tab_rearranged(idx_to):
 
 func _on_fight_started():
 	tab_bar.visible = false
-	_tab_before_fight = tab_bar.current_tab
 	tab_bar.current_tab = 2
 
 
 func _on_fight_ended():
 	tab_bar.visible = true
-	tab_bar.current_tab = _tab_before_fight
