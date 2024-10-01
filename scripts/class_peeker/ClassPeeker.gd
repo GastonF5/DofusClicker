ca017cdfb52e4e060f368365c77891cdae378728
GastonF5 <fr.gaston5@gmail.com>
2024-10-01 14:40:20 +0200

    Correction libellé Crâ

--- scripts/class_peeker/ClassPeeker.gd
+++ scripts/class_peeker/ClassPeeker.gd
@@ -5,7 +5,7 @@ const ASSETS_PATH = "res://assets/classes/logo/"
 
 var classes = {
 	1: "Féca", 2: "Osamodas", 3: "Enutrof", 4: "Sram", 5: "Xelor", 6: "Ecaflip",
-	7: "Eniripsa", 8: "Iop", 9: "Cra", 10: "Sadida", 11: "Sacrieur", 12: "Panda",
+	7: "Eniripsa", 8: "Iop", 9: "Crâ", 10: "Sadida", 11: "Sacrieur", 12: "Panda",
 	13: "Roublard", 14: "Zobal", 15: "Steamer", 16: "Eliotrope", 17: "Huppermage",
 	18: "Ouginak", 19: "Forgelance"
 }
