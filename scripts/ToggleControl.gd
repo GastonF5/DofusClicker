7b4027fefacbafa9795805ae60484cc5d0679ff9
GastonF5 <fr.gaston5@gmail.com>
2024-10-03 13:29:26 +0200

    #102 Ajout icône flèche

--- scripts/ToggleControl.gd
+++ scripts/ToggleControl.gd
@@ -1,5 +1,7 @@
 class_name ToggleControl extends Control
 
+const arrow_down = preload("res://assets/icons/green_arrow_down.png")
+const arrow_right = preload("res://assets/icons/green_arrow_right.png")
 
 @export var button: Button
 @export var content: Control
@@ -21,9 +23,11 @@ func toggle_content(toggled: bool):
 	var tween = create_tween()
 	var child = content.get_child(0)
 	if toggled:
+		button.icon = arrow_down
 		content.visible = true
 		tween.tween_property(content, "custom_minimum_size", child.size, 0.4).set_trans(Tween.TRANS_CIRC)
 	else:
+		button.icon = arrow_right
 		tween.tween_property(content, "custom_minimum_size", Vector2(child.size.x, 0), 0.4).set_trans(Tween.TRANS_CIRC)
 		await tween.finished
 		content.visible = true
