6e37a53fbe7aa41d67b094e1904c8591f9ced0ab
GastonF5 <fr.gaston5@gmail.com>
2024-10-14 19:16:56 +0200

    Await inutile

--- scripts/manager/GameManager.gd
+++ scripts/manager/GameManager.gd
@@ -74,8 +74,6 @@ func init_game(save_res: SaveResource = null):
 	if save_res:
 		if !SaveManager.load_save(save_res):
 			return false
-	if !RecipeManager.is_initialized:
-		await RecipeManager.initialized
 	class_peeker.visible = false
 	in_game = true
 	log.info("Initialisation effectuée avec succès")
