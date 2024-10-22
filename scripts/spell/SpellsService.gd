c1ed8e2cbc47098e4ed23f8a48e1ebd54ed69a8e
GastonF5 <fr.gaston5@gmail.com>
2024-10-22 13:55:04 +0200

    Unselect uniquement si l'entité a bien changé de place

--- scripts/spell/SpellsService.gd
+++ scripts/spell/SpellsService.gd
@@ -246,7 +246,8 @@ static func perform_poussee(caster: Entity, target: Entity, effect: EffectResour
 	if dist_between_plates > 0:
 		destination_plate = plate.call(direction_callable_name, dist_between_plates)
 		target.reparent(destination_plate, false)
-		target.unselect()
+		if destination_plate != plate:
+			target.unselect()
 	target.animate_poussee(get_direction(direction), dist_between_plates)
 
 
